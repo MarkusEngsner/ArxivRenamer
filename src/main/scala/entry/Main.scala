3ea86450ab4de7cc1302222d10b3435bbb19d75a
Markus Engsner <markus.engsner@gmail.com>
2020-07-31 10:58:43 +0200

    Implement CLI interface

--- src/main/scala/entry/Main.scala
+++ src/main/scala/entry/Main.scala
@@ -7,15 +7,20 @@ object Main {
   def renameFile(oldName: String) =
     copyFile(oldName, newFileName(oldName))
 
-  def renameAll() = arxivPdfs foreach renameFile
+  def renameArray(arr: Array[String]) = arr foreach renameFile
 
+  def renameAll() = renameArray(arxivPdfs())
+
+  /* Usage:
+    First argument: all or list of filenames
+   */
   def main(args: Array[String]): Unit = {
     println(System.getProperty("user.dir"))
     if (args.length < 1) {
       println("too few arguments")
       return
     }
-    val oldName = args(0)
-    renameFile(oldName)
+    if (args(0) == "all") renameAll()
+    else renameArray(args)
   }
 }
